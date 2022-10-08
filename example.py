d50b44da30635d1d04da79f29b22092ab638322e
MariaTolkacheva <mari.tolkacheva.04@mail.ru>
2022-10-08 23:33:48 +0300

    nnew

--- example.py
+++ example.py
@@ -1,15 +1,3 @@
-def add(a, b):
-    return a + b
-
-
-def test_add():
-    assert add(2, 3) == 5
-    assert add('space', 'ship') == 'spaceship'
-
-
-def subtract(a, b):
-    return a + b  # <--- fix this in step 7
-
 from utils import division
 import pytest
 
