59a4076936e1c3f2f456ecc675477a9a16d73d24
MariaTolkacheva <mari.tolkacheva.04@mail.ru>
2022-10-09 20:52:22 +0300

    dont know

---
+++ .github/workflows/utils.py
@@ -0,0 +1,25 @@
+# opening the file
+f = open(r'C:\Users\Huawei NB\TZ2\tests\numbers.txt', 'r')
+# a list with all our numbers
+str_num = []
+for x in f:
+    str_num += x.split()
+
+
+def num_min(list_num):
+    return min(int(x) for x in list_num)
+
+
+def num_max(list_num):
+    return max(int(x) for x in list_num)
+
+
+def num_sum(list_num):
+    return sum(int(x) for x in list_num)
+
+
+def num_prod(a):
+    s = 1
+    for i in a:
+        s *= int(i)
+    return s
