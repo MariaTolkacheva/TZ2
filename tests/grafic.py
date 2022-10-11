97799f77392cba9b64bdf601e59688290a53c94b
MariaTolkacheva <mari.tolkacheva.04@mail.ru>
2022-10-11 21:13:46 +0300

    чистка

--- tests/grafic.py
+++ tests/grafic.py
@@ -14,29 +14,12 @@ for i in range(1,1000):
     num_sum(set)
     num_max(set)
     end_time = time.time()
-    time_data.append((end_time-start_time)*100)
+    time_data.append((end_time-start_time)*1000)
 print(time_data)
 ypoints = np.array(time_data)
 xpoints = np.array([i for i in range(len(time_data))])
 
-plt.plot(xpoints, ypoints)
-plt.show()
-
-
-
-
-x = np.arange(-2 * np.pi, 2 * np.pi, 0.01)
-y = np.sin(3 * x) / x
-plt.plot(x, y)
-plt.show()
-
-import pandas as pd
-
-data = {'series2': [2, 4, 5, 2, 4],
-        'series3': [3, 2, 3, 1, 3]}
-df = pd.DataFrame(data)
-x = np.arange(5)
-plt.axis([0, 5, 0, 7])
-plt.plot(x, df)
-plt.legend(data, loc=2)
+plt.plot(xpoints, ypoints,'o')
+plt.xlabel("количество цифр")
+plt.ylabel("Примерное время выполнения")
 plt.show()
